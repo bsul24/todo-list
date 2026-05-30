18211536f0b5064dc58e95288183db68eb529035
Bill Sullivan <w.o.sullivan24@gmail.com>
2026-05-30 15:47:37 -0700

    Create initial file structure
    Setup the project as a webpack project, get all the webpack config
    files setup and the package.json setup. Create src folder with initial
    files and a modules folder.

---
+++ src/modules/Todo.js
@@ -0,0 +1,10 @@
+export default class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.id = crypto.randomUUID()
+    this.title = title
+    this.description = description
+    this.dueDate = dueDate
+    this.priority = priority
+    this.completed = false
+  }
+}
